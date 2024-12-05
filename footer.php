44824d336233f2def5da9f07f18e7aa7e14d7f39
Jasmeen Khanam <147890118+Jasmeen8077@users.noreply.github.com>
2024-12-05 13:35:34 +0530

    footer design change

--- footer.php
+++ footer.php
@@ -1,120 +1,134 @@
 <footer class="footer-area footer-bg">
-    <div class="container ">
-        <div class="footer-top pt-40 pb-40 ">
-            <div class="row align-items-center justify-content-center ">
-                <div class="col-lg-3 col-md-6 ">
-                    <div class="footer-widget ">
-                        <div class="footer-logo ">
-                            <a href="index.html ">
-                                <img src="assets/img/logos/flogo.png " style="width: 250px; " alt="Images " />
+    <div class="container">
+        <div class="footer-top pt-40 pb-40">
+            <div class="row align-items-center justify-content-center">
+                <!-- Footer Widget 1 -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-widget">
+                        <div class="footer-logo">
+                            <a href="index.html">
+                                <img src="assets/img/logos/flogo.png" style="width: 250px;" alt="Images" />
                             </a>
-                            <p class="mt-2 ">
-                                a winter vacation retreat that promises enchanting experiences and breathtaking beauty. Nestled in this charming city, you'll discover an array of activities and attractions to make your winter getaway truly memorable donec risus.
+                            <p class="mt-2">
+                                Nestled on the mountain side of The San Juan River Village, this lovely mountain home with reliable high-speed Starlinks wifi has a secluded setting and a magnificent panoramic view of the San Juan Mountains.
                             </p>
                         </div>
+                    </div>
+                </div>
+                <!-- Footer Widget 2 -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-widget">
+                        <div class="footer-logo">
 
+                            <h3 class="mt-3">Links</h3>
+                            <p class="mt-2">
+                            <ul class="footer-list ">
+                                <li>
+                                    <a href="about-us.php " target="_blank ">
+                                        <i class="bx bx-caret-right "></i> About Us
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="rooms.php " target="_blank ">
+                                        <i class="bx bx-caret-right "></i> Rooms
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="gallery.php" target="_blank ">
+                                        <i class="bx bx-caret-right "></i> Gallery
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="service.php" target="_blank ">
+                                        <i class="bx bx-caret-right "></i> Services
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="contact.php" target="_blank ">
+                                        <i class="bx bx-caret-right "></i> Contact us
+                                    </a>
+                                </li>
+                            </ul>
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 ">
-                    <div class="footer-widget pl-5 ">
-                        <h3>Links</h3>
-                        <ul class="footer-list ">
-                            <li>
-                                <a href="about-us.php " target="_blank ">
-                                    <i class="bx bx-caret-right "></i> About Us
-                                </a>
-                            </li>
-                            <li>
-                                <a href="rooms.php " target="_blank ">
-                                    <i class="bx bx-caret-right "></i> Rooms
-                                </a>
-                            </li>
-                            <li>
-                                <a href="gallery.php" target="_blank ">
-                                    <i class="bx bx-caret-right "></i> Gallery
-                                </a>
-                            </li>
-                            <li>
-                                <a href="service.php" target="_blank ">
-                                    <i class="bx bx-caret-right "></i> Services
-                                </a>
-                            </li>
-                            <li>
-                                <a href="contact.php" target="_blank ">
-                                    <i class="bx bx-caret-right "></i> Contact us
-                                </a>
-                            </li>
-                        </ul>
+                <!-- Footer Widget 3 -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-widget">
+                        <div class="footer-logo">
+
+                            <h3 class="mt-3">Contact Us</h3>
+                            <p class="mt-2">
+                            <ul class="footer-list-contact ">
+                                <li>
+                                    <i class="bx bx-home-alt "></i>
+                                    <a href="javascript:void(0);">Colorado</a>
+                                </li>
+                                <li>
+                                    <i class="bx bx-phone-call "></i>
+                                    <a href="tel:(626)482-7900">(626)
+                                        482-7900</a>
+                                </li>
+                                <li>
+                                    <i class="bx bx-envelope "></i>
+                                    <a href="mailto:codopagosa@earthlink.net"><span
+                                            class=" ">codopagosa@earthlink.net</span></a>
+                                </li>
+                            </ul>
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 ">
-                    <div class="footer-widget ">
-                        <h3>Contact Us</h3>
-                        <ul class="footer-list-contact ">
-                            <li>
-                                <i class="bx bx-home-alt "></i>
-                                <a href="javascript:void(0);">Colorado</a>
-                            </li>
-                            <li>
-                                <i class="bx bx-phone-call "></i>
-                                <a href="tel:(626)482-7900">(626)
-                                    482-7900</a>
-                            </li>
-                            <li>
-                                <i class="bx bx-envelope "></i>
-                                <a href="mailto:codopagosa@earthlink.net"><span
-                                        class=" ">codopagosa@earthlink.net</span></a>
-                            </li>
-                        </ul>
+                <!-- Footer Widget 4 -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-widget">
+                        <div class="footer-logo">
+
+                            <h3 class="mt-3">Download</h3>
+                            <p class="mt-2">
+                            <ul class="footer-list-contact ">
+                                <li>
+                                    <i class="bx bx-file "></i>
+                                    <a href="assets/download/2017-2018_Booking_Agreement_Serendipity.pdf"><span
+                                            class=" ">Click Here to Download Reservation Form</span></a>
+                                </li>
+                            </ul>
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 ">
-                    <div class="footer-widget ">
-                        <h3>Download</h3>
-                        <ul class="footer-list-contact ">
-                            <li>
-                                <i class="bx bx-file "></i>
-                                <a href="assets/download/2017-2018_Booking_Agreement_Serendipity.pdf"><span
-                                        class=" ">Click Here to Download Reservation Form</span></a>
+            </div>
+        </div>
+        <!-- Social Links and Copy Right Section -->
+        <div class="copy-right-area">
+            <div class="row align-items-center">
+                <!-- Copyright Text -->
+                <div class="col-lg-6 col-md-6 text-start">
+                    <p class="text-light">
+                        © <span>Copyright 2024 by <a href="https://www.apexwebsoft.com/" class="text-light">Apex Websoft</a>. All Rights Reserved.</span>
+                    </p>
+                </div>
+                <!-- Social Media Icons -->
+                <div class="col-lg-6 col-md-6 text-end">
+                    <div class="social-icon">
+                        <ul class="social-link list-inline mb-0">
+                            <li class="list-inline-item">
+                                <a href="https://www.facebook.com/login/" target="_blank"><i class="bx bxl-facebook"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="https://twitter.com/i/flow/login" target="_blank"><i class="bx bxl-twitter"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="https://www.instagram.com/accounts/login/" target="_blank"><i class="bx bxl-instagram"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="https://www.youtube.com/watch?v=zWPsjhBaRb0" target="_blank"><i class="bx bxl-youtube"></i></a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="copy-right-area ">
-            <div class="row justify-content-center ">
-                <div class="col-lg-8 col-md-8 ">
-                    <div class="copy-right-text text-align1 ">
-                        <p>
-                            © <span>Copyright 2024 by <a href="https://www.apexwebsoft.com/"> Apex Websoft</a> All Right Reserved.</span>
 
-                        </p>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 ">
-                    <div class="social-icon text-align2 ">
-                        <ul class="social-link ">
-                            <li>
-                                <a href="https://www.facebook.com/login/ " target="_blank "><i
-                                        class="bx bxl-facebook "></i></a>
-                            </li>
-                            <li>
-                                <a href="https://twitter.com/i/flow/login " target="_blank "><i
-                                        class="bx bxl-twitter "></i></a>
-                            </li>
-                            <li>
-                                <a href="https://www.instagram.com/accounts/login/ " target="_blank "><i
-                                        class="bx bxl-instagram "></i></a>
-                            </li>
-                            <li>
-                                <a href="https://www.youtube.com/watch?v=zWPsjhBaRb0 " target="_blank "><i
-                                        class="bx bxl-youtube "></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 </footer>
