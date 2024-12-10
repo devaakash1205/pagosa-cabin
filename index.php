6698de85e61d54bbc6aef0f3ddd5b61c836cee71
Jasmeen Khanam <147890118+Jasmeen8077@users.noreply.github.com>
2024-12-10 21:41:13 +0530

    service remove

--- index.php
+++ index.php
@@ -64,67 +64,6 @@ include("header.php");
     </div>
 </div>
 
-<!--Services-->
-<div class="blog-area pb-70 pt-100">
-    <div class="container">
-        <div class="section-title text-center">
-            <span>Services</span>
-            <h2>Our Services</h2>
-        </div>
-        <div class="row pt-45">
-            <!-- First Blog Card -->
-            <div class="col-lg-6 d-flex">
-                <div class="blog-card d-flex flex-column" style="flex-grow: 1;">
-                    <div class="row align-items-center justify-content-center h-100">
-                        <div class="col-lg-5 col-md-4 p-0">
-                            <div class="blog-img">
-                                <a href="blog-details.html">
-                                    <img src="assets/img/gallery/house-2.jpg" alt="Serendipity Living" class="img-fluid">
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-lg-7 col-md-8 p-0">
-                            <div class="blog-content">
-                                <h3>
-                                    <a href="blog-details.html">Serendipity Living and Dining</a>
-                                </h3>
-                                <p>Experience the charm of elegant living spaces and the joy of fine dining. A perfect retreat for relaxation and luxury.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Second Blog Card -->
-            <div class="col-lg-6 d-flex">
-                <div class="blog-card d-flex flex-column" style="flex-grow: 1;">
-                    <div class="row align-items-center justify-content-center h-100">
-                        <div class="col-lg-5 col-md-4 p-0">
-                            <div class="blog-img">
-                                <a href="blog-details.html">
-                                    <img src="assets/img/service/service-1.jpg" alt="Beautiful View" class="img-fluid">
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-lg-7 col-md-8 p-0">
-                            <div class="blog-content">
-                                <h3>
-                                    <a href="service.php">A Beautiful View from Serendipity</a>
-                                </h3>
-                                <p>Indulge in stunning panoramic views that surround you, offering a peaceful and scenic escape to recharge your senses.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="text-center">
-                <a href="service.php" class="default-btn btn-bg-one border-radius-5" style="text-decoration:none;">
-                    Read More
-                </a>
-            </div>
-        </div>
-    </div>
-</div>
-
 <!--About Us-->
 <div class="about-area pt-70 pb-70">
     <div class="container-fluid">
@@ -336,4 +275,5 @@ include("footer.php");
 include("script.php");
 ?>
 </body>
+
 </html>
